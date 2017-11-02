876e2baa0ea66f836b06041c8528f35f0eb547ef
C3NZ <epichdd@gmail.com>
2017-11-02 17:05:29 -0400

    Initial commit

---
+++ Logger.py
@@ -0,0 +1,118 @@
+'''
+Module - logger.py
+
+Description - Module for logging information to the console or txt file
+
+Items -
+
+    Class Logger:
+        -Logger Description-
+            This class is used for writing text to either the console or txt file.
+            You do not instantiate any Logger objects for use but rather call each function
+            statically.
+
+        -Logger  static variables-
+            TYPE : NAME - DESC
+            string : directory - directory the log files will be written to
+            string : fileName - The name of the log file
+            file object : f - File object for the logger to write to
+            boolean : forceConsole - Forces log_* functions to print to the console
+                                     as well as write to a txt file.
+
+
+        -Logger functions-
+            RETURNTYPE : NAME - DESC
+            void : console_i - prints information to the console
+            void : console_e - prints error information to the console
+            void : console_d - prints debug information to the console
+            void : log_info - writes information to a txt file
+            void : log_error - writes error information to a txt file
+            void : log_debug - writes debug information to a txt file
+            void : close_log - Closes the file object
+
+'''
+
+from inspect import getframeinfo, stack
+from constants import DEBUGMODE
+from time import strftime
+from os.path import abspath, join
+
+#Class to log information to both a text file and the users console
+class Logger(object):
+    #Static class variables
+    directory = "utils/logs/"
+    fileName = "{}.txt".format(strftime("%m-%d-%Y"))
+    f = open(join(directory, fileName), "a+")
+    forceConsole = True
+
+    '''
+       If the console is only being called explicitly, the only
+       argument that needs to be passed is a string containing the
+       information you're trying to print out
+
+       arg[0] = message - output to the console
+       arg[1] = file name - what file the logger instance was called from
+       arg[2] = line number - what linea logger instance was called from
+
+    '''
+
+    #Write info to the console
+    @staticmethod
+    def console_i(*args):
+        if len(args) == 1:
+            caller = getframeinfo(stack()[1][0])
+            print("[INFO][{}][{}]:{}".format(caller.filename, caller.lineno, args[0]))
+        else:
+            print("[INFO][{}][{}]:{}".format(args[2], args[1], args[0]))
+
+    #Write an error to the console
+    @staticmethod
+    def console_e(*args):
+        if len(args) == 1:
+            caller = getframeinfo(stack()[1][0])
+            print("[ERROR][{}][{}]:{}".format(caller.filename, caller.lineno, args[0]))
+        else:
+            print("[ERROR][{}][{}]:{}".format(args[2], args[1], args[0]))
+
+    #Write debug info to the console
+    @staticmethod
+    def console_d(*args):
+        if DEBUGMODE:
+            if len(args) == 1:
+                caller = getframeinfo(stack()[1][0])
+                print("[DEBUG][{}][{}]:{}".format(caller.filename, caller.lineno, args[0]))
+            else:
+                print("[DEBUG][{}][{}]:{}".format(args[2], args[1], args[0]))
+
+    #append information to a log file
+    @staticmethod
+    def log_info(message):
+        caller = getframeinfo(stack()[1][0])
+        if Logger.forceConsole:
+            Logger.console_i(message, caller.filename, caller.lineno)
+        Logger.f.write("[INFO][{}][{}][{}][{}]:{}\n".format(strftime("%m-%d-%Y"),
+                        strftime("%H:%M:%S"), caller.filename, caller.lineno, message))
+
+    #append error information to a log file
+    @staticmethod
+    def log_error(message):
+        caller = getframeinfo(stack()[1][0])
+        if Logger.forceConsole:
+            Logger.console_e(message, caller.filename, caller.lineno)
+        Logger.f.write("[ERROR][{}][{}][{}][{}]:{}\n".format(strftime("%m-%d-%Y"),
+                        strftime("%H:%M:%S"), caller.filename, caller.lineno, message))
+
+    #append debugging information to a log file
+    @staticmethod
+    def log_debug(message):
+        caller = getframeinfo(stack()[1][0])
+        if DEBUGMODE:
+            if Logger.forceConsole:
+                Logger.console_d(message, caller.filename, caller.lineno)
+            Logger.f.write("[DEBUG][{}][{}][{}][{}]:{}\n".format(strftime("%m-%d-%Y"),
+                            strftime("%H:%M:%S"), caller.filename, caller.lineno, message))
+
+    #Close the logging file when it no longer needs to be accessed
+    @staticmethod
+    def close_log():
+        Logger.f.close()
---
+++ README.md
@@ -0,0 +1,4 @@
+# Logger
+## A simple, lightweight, static logging utility
+
+Logger aims to simplify logging while maintaining a relatively small code base. This is meant for smaller projects at the time but I have plans to scale it to suite larger application needs.
